2a37e557ccb409a985edb562ab9aef4301afe218
Peter Barcsak <peter.barcsak@gmail.com>
2017-04-14 14:37:25 +0100

    Adds private methods - challenge 15 done

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,21 +1,34 @@
-class DockingStation < Bike
+class DockingStation
 
-attr_reader :docked
+  attr_reader :bikes
 
-def initialize
- @docked = docked
-end
-
-def release_bike
-    Bike.new
-    self.docked = false
+  def initialize
+    @bikes = []
+    20.times do
+      @bikes << Bike.new
+    end
   end
-end
 
-def return_bike
-    #Bike.new
-    @docked = true
+  def release_bike
+    return raise "No bike available" if empty?
+    @bikes.pop
   end
+
+  def dock(bike)
+    return raise "Docking station is full" if full?
+    @bikes << bike
+  end
+
+  private
+    def full?
+      return true if @bikes.count == 20
+      false
+    end
+
+    def empty?
+      return true if @bikes.count == 0
+      false
+    end
 end
 
 class Bike
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,15 +1,33 @@
 require 'docking_station'
 
 describe DockingStation do
-  describe ".release_bike" do
-  station = DockingStation.new
-  bike = station.release_bike
-  it "should be an instace of bike" do
-  bike.should be_an_instance_of Bike
+   it 'releases 20 bikes per station' do
+    docking_station = DockingStation.new
+    expect { 21.times { subject.release_bike } }.to raise_error("No bike available")
+   end
+
+  describe "#release_bike" do
+    it "should be an instace of bike" do
+      Bike.class
+    end
+    it 'releases working bikes' do
+      expect(subject.release_bike).to be_working
+    end
   end
-end
-  it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+  describe "#dock" do
+    it "should dock the bike by returning its instance" do
+      bike = Bike.new
+      station = DockingStation.new
+      station.release_bike
+      expect { station.dock(bike) }.to_not raise_error 
+      # p station
+      # p station.dock(bike)
+    end
+    it "shouldn't let someone dock a bike when it's full" do
+      station = DockingStation.new
+      bike = station.release_bike
+      station.dock(bike)
+      expect{subject.dock(bike)}.to raise_error "Docking station is full"
+    end
   end
 end
